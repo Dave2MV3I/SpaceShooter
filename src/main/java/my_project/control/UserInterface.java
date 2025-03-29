32c0660f8b44647036b6f8be98716dd876fb9cb6
Dave2MV3I <dagl777-github@yahoo.com>
2025-03-29 20:59:59 +0100

    Buttons eingezeichnet, Arrays und Booleans dafür. Interaktion und Buttonbeschriftung/Icons noch zu machen.

--- src/main/java/my_project/control/UserInterface.java
+++ src/main/java/my_project/control/UserInterface.java
@@ -7,28 +7,86 @@ import my_project.model.menu.MenuButton;
 public class UserInterface extends GraphicalObject{
 
     // Attribute
-    private int nSettings = 4;
+        private boolean menuVisible = false;
+
     // Arrays
-    private MenuButton[] activeSettings = new MenuButton[nSettings]; //Music, Level, lvlTimer, glbTimer
-    private MenuButton[] inactiveSettings = new MenuButton[0];
+        private final String[] visiblePaths = {
+                "src/main/resources/graphic/menu/button.png", // MUSIC
+                "src/main/resources/graphic/menu/button.png", // LEVEL
+                "src/main/resources/graphic/menu/button.png", // LEVEL_TIMER
+                "src/main/resources/graphic/menu/button.png",  // GLOBAL_TIMER
+                "src/main/resources/graphic/menu/button.png"  // SHIELD
+        };
+        private final String[] invisiblePaths = {
+                "src/main/resources/graphic/menu/button.png", //
+                "src/main/resources/graphic/menu/button.png", //
+                "src/main/resources/graphic/menu/button.png", //
+                "src/main/resources/graphic/menu/button.png",  //
+                "src/main/resources/graphic/menu/button.png"  //
+        };
+        private MenuButton[] buttons = new MenuButton[visiblePaths.length + invisiblePaths.length];
 
     // Referenzen
 
 
     // Methoden
     public UserInterface() {
-        for (int i = 0; i < nSettings; i++) {
-            activeSettings[i] = new MenuButton(20, 20+i*40, "src/main/resources/graphic/menu/button.png");
+        double width = 120;
+        double height = 30;
+
+        // So viele Buttons erstellen, wie es Pfade gibt, richtiger Pfad als Parameter übergeben; buttons[] mit ihnen befüllen
+        for (int i = 0; i < visiblePaths.length; i++) {
+            buttons[i] = new MenuButton(20, 20+i*(height+10), visiblePaths[i], width, height, true, true);
+        }
+        for (int i = 0; i < invisiblePaths.length; i++) {
+            buttons[visiblePaths.length+i] = new MenuButton(20, (20+visiblePaths.length*(height+10))+(20+i*(height+10)), visiblePaths[i], width, height, true, false);
         }
     }
 
     @Override
-    public void update(double dt){}
+    public void update(double dt){
+    }
 
     @Override
     public void draw(DrawTool drawTool){
-        for (int i = 0; i < nSettings; i++) {
-            activeSettings[i].draw(drawTool);
+        for (MenuButton button : buttons) {
+            if (button.isButtonVisible()) button.draw(drawTool);
+        }
+    }
+
+    // Wenn man die Maus über den Methodenbezeichner changeSettingStatus hovert, sieht man folgenden Kommentar:
+    /**
+     * Methode ändert den Status einer bestimmten Einstellung.
+     *
+     * @param index Die Indexe der Einstellungen:<br>
+     *              0 = MUSIC<br>
+     *              1 = LEVEL<br>
+     *              2 = LEVEL_TIMER<br>
+     *              3 = GLOBAL_TIMER<br>
+     *              4 = SHIELD<br>
+     */
+
+    public void changeSettingStatus(int index) {
+        if (index >= 0 && index < buttons.length && buttons[index] != null) {
+            buttons[index].setButtonVisible(!buttons[index].isButtonVisible());
+        } else {
+            System.err.println("Fehler: Ungültiger Index " + index); //Für Fehlerausgaben System.err verwenden statt System.out
+        }
+    }
+
+
+    public void handleMenuClicked(boolean opened){
+        if (opened){
+            for (MenuButton button : buttons) {
+                button.setButtonVisible(true);
+            }
+        } else {
+            for (MenuButton button : buttons) {
+                button.setButtonVisible(false);
+            }
+            //Auch Positionen neuanordnen!
+            //...
         }
     }
 }
+
--- src/main/java/my_project/model/menu/MenuButton.java
+++ src/main/java/my_project/model/menu/MenuButton.java
@@ -4,15 +4,54 @@ import my_project.model.Picture;
 import KAGO_framework.view.DrawTool;
 
 public class MenuButton extends Picture {
-    public MenuButton(double x, double y, String path) {super(x, y, path);}
+    private double radius; // Nicht der Radius des GraphicalObjects, deshalb neu deklariert
+
+    private boolean settingActive; // Die Einstellung ist aktiviert, z.B. BG-Musik
+    private boolean buttonVisible; // Der Button dieser Einstellung wird angezeigt
+
+    public MenuButton(double x, double y, String path, double width, double height, boolean settingActive, boolean buttonVisible) {
+        super(x+0.5*height, y, path); //Icon bekommt eigene Koordinaten
+        this.width = width;
+        this.height = height;
+        this.radius = 0.5*height;
+        this.settingActive = settingActive;
+        this.buttonVisible = buttonVisible;
+    }
 
     @Override
     public void update(double dt) {}
 
     @Override
     public void draw(DrawTool drawTool) {
-        drawTool.setCurrentColor(0,0,255,255);
-        drawTool.drawFilledRectangle(x, y, 120,30);
+        // Button zeichnen
+        if (settingActive) { drawTool.setCurrentColor(0,0,255,255);}
+        else drawTool.setCurrentColor(0,0,255,100);
+
+        drawTool.drawFilledCircle(x+radius, y+radius, radius);
+        drawTool.drawFilledRectangle(x+radius, y, width-height, height);
+        drawTool.drawFilledCircle(x+width-radius, y+radius, radius);
+
+        // Icon zeichnen
+        super.draw(drawTool);
+
+        // Text zeichnen
+
     }
+
+    public boolean isSettingActive() {
+        return settingActive;
+    }
+    public void setSettingActive(boolean settingActive) {
+        this.settingActive = settingActive;
+    }
+
+    public boolean isButtonVisible() {
+        return buttonVisible;
+    }
+    public void setButtonVisible(boolean buttonVisible) {
+        this.buttonVisible = buttonVisible;
+    }
+
+
 
 }
